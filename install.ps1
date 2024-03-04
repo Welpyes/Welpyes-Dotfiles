8a58e3cb47477b522cdf2aa289f65622a93b7557
Welpyes <110968684+Welpyes@users.noreply.github.com>
2024-03-04 10:13:32 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -24,5 +24,5 @@ Clear-Host
 Write-Host "To run the status bar you can just run the run-silent.vbs file" -ForegroundColor Blue
 Write-Host "You can also make your own batch file if you can" -ForegroundColor Blue
 Write-Host "i dont really know the dependencies of CAVA but you can go to their github page to know" -ForegroundColor Blue
-
+Remove-Item -Path "$env:USERPROFILE\welpyes-dotfiles\install.ps1"
 Set-Location -Path "$env:USERPROFILE"
