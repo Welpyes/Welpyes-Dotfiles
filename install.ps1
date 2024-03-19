e8cd23d1180068a53d509e088de087ffcf7c5f25
Welpyes <110968684+Welpyes@users.noreply.github.com>
2024-03-19 09:30:42 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -18,6 +18,7 @@ if ($response -eq 'y') {
     git clone https://github.com/Welpyes/welpyes-yasb
     Set-Location -Path "$env:USERPROFILE\welpyes-yasb"
     pip install -r requirements.txt
+    Clear-Host
     Write-Host "To run the status bar you can just run the run-silent.vbs file" -ForegroundColor Blue
     Write-Host "You can also make your own batch file if you can" -ForegroundColor Blue
     Write-Host "i dont really know the dependencies of CAVA but you can go to their github page to know" -ForegroundColor Blue
