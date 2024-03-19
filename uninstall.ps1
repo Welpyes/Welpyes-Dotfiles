cecadd591a3ca4e56acfb5eff855b7e43f7e7b4a
Welpyes <110968684+Welpyes@users.noreply.github.com>
2024-03-19 09:40:00 +0800

    Add files via upload

---
+++ uninstall.ps1
@@ -0,0 +1,14 @@
+$question = "This script uninstalls yasb and its python dependencies do you want to proceed? (y/n)"
+$response = Read-Host -Prompt $question
+
+if ($response -eq 'y') {
+    Write-Host "Proceeding with the uninstallation..."
+    Set-Location -Path "$env:USERPROFILE\welpyes-yasb"
+    pip uninstall -r -y requirements.txt
+    Remove-Item -Recurse -Force "$env:USERPROFILE\welpyes-yasb"
+    Remove-Item -Recurse -Force "$env:USERPROFILE\welpyes-dotfiles"
+    Remove-Item -Recurse -Force "$env:USERPROFILE\.yasb"
+} else {
+    Write-Host "uninstallation cancelled."
+}
+
