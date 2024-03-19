156d516417888fb9865b39a40b5f8d1652c30a35
Welpyes <110968684+Welpyes@users.noreply.github.com>
2024-03-19 15:51:20 +0800

    Add files via upload

---
+++ scoop-install.ps1
@@ -0,0 +1,9 @@
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+
+.$profile
+scoop update
+scoop bucket add extras
+scoop bucket add nerd-fonts
+
+scoop install git python jetbrainsmono-nf
