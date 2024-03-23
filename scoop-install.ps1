dfbe4fb73b87d359cf9046edce8a71d269d664f1
Welpyes <110968684+Welpyes@users.noreply.github.com>
2024-03-23 23:52:06 +0800

    Update scoop-install.ps1

--- scoop-install.ps1
+++ scoop-install.ps1
@@ -1,8 +1,9 @@
 irm -Uri https://get.scoop.sh | iex
 
 .$profile
+scoop install git
 scoop update
 scoop bucket add extras
 scoop bucket add nerd-fonts
 
-scoop install git python jetbrainsmono-nf
+scoop install python jetbrainsmono-nf
